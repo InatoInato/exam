d23da44b28b28aa7fc36c99f576ee5179705afce
InatoInato <diasmuratbekff@gmail.com>
2024-05-29 13:33:28 +0500

    connecting

--- front/src/pages/Login.vue
+++ front/src/pages/Login.vue
@@ -10,10 +10,22 @@ export default{
         <h1>Login</h1>
         <input type="email" placeholder="Enter your email">
         <input type="password" placeholder="Enter your password">
-        <button type="submit">Login</button>
+        <div class="button">
+            <button type="submit" class="but">Login</button>
+            <select id="login_as" class="but">
+                <option selected disabled>Войти как</option>
+                <option value="runner">Бегун</option>
+                <option value="">Координатор</option>
+                <option value="">Администратор</option>
+            </select>
+        </div>
     </div>
 </main>
 </template>
 
 <style>
+.but{
+    margin: 10px 30px;
+    width: 100px;
+}
 </style>
